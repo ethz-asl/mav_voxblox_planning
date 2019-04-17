643200f869c887fa9ea19e5050e48a2a7dc13360
Helen Oleynikova <helenoleynikova@gmail.com>
2019-04-17 11:22:05 +0200

    Sharticles compile.

--- voxblox_loco_planner/include/voxblox_loco_planner/shotgun_planner.h
+++ voxblox_loco_planner/include/voxblox_loco_planner/shotgun_planner.h
@@ -2,12 +2,15 @@
 #define VOXBLOX_LOCO_PLANNER_SHOTGUN_PLANNER_H_
 
 #include <mav_planning_common/physical_constraints.h>
+#include <voxblox/core/esdf_map.h>
 
 namespace mav_planning {
 
 struct ShotgunParameters {
   // How large a step, in meters, to make.
   float max_step_size = 1.0;
+  // Whether to take 1 voxel (false) or multi-voxel (true) steps.
+  bool take_large_steps = false;
 
   // Probabilities (must sum up to < 1 together) that the particle will do one
   // of the following things. Rest of the probability is just random motion.
@@ -26,7 +29,7 @@ class ShotgunPlanner {
   ShotgunPlanner(const ShotgunParameters& params) : params_(params) {}
 
   const ShotgunParameters& getParams() const { return params_; }
-  void setParams(const ShotgunParameters& params) { params = params_; }
+  void setParams(const ShotgunParameters& params) { params_ = params; }
 
   const PhysicalConstraints& getPhysicalConstraints() const {
     return constraints_;
@@ -42,6 +45,7 @@ class ShotgunPlanner {
   // Main function to call. Returns whether the particles were able to get
   // anywhere at all.
   bool shootParticles(int num_particles, int max_steps,
+                      const Eigen::Vector3d& start, const Eigen::Vector3d& goal,
                       Eigen::Vector3d* best_goal,
                       voxblox::AlignedVector<Eigen::Vector3d>* best_path);
 
--- voxblox_loco_planner/src/shotgun_planner.cpp
+++ voxblox_loco_planner/src/shotgun_planner.cpp
@@ -1,4 +1,7 @@
+#include <limits>
+
 #include <mav_planning_common/utils.h>
+#include <voxblox/utils/neighbor_tools.h>
 
 #include "voxblox_loco_planner/shotgun_planner.h"
 
@@ -23,10 +26,73 @@ bool ShotgunPlanner::shootParticles(
   if (!esdf_map_) {
     return false;
   }
+  voxblox::Layer<voxblox::EsdfVoxel>* layer = esdf_map_->getEsdfLayerPtr();
+  CHECK_NOTNULL(layer);
+  float voxel_size = layer->voxel_size();
 
   bool success = false;
+
+  // Figure out the voxel index of the start point.
+  voxblox::GlobalIndex start_index =
+      voxblox::getGridIndexFromPoint<voxblox::GlobalIndex>(
+          start.cast<voxblox::FloatingPoint>(), voxel_size);
+  voxblox::GlobalIndex goal_index =
+      voxblox::getGridIndexFromPoint<voxblox::GlobalIndex>(
+          goal.cast<voxblox::FloatingPoint>(), voxel_size);
+
+  voxblox::GlobalIndex current_index, last_index, best_index;
+  double best_distance = std::numeric_limits<double>::max();
+  voxblox::Neighborhood<>::IndexMatrix neighbors;
+
   for (int n_particle = 0; n_particle < num_particles; n_particle++) {
+    current_index = start_index;
     for (int step = 0; step < max_steps; step++) {
+      // Get the neighbors of the current index.
+      voxblox::Neighborhood<>::getFromGlobalIndex(current_index, &neighbors);
+
+      voxblox::AlignedVector<voxblox::GlobalIndex> valid_neighbors;
+
+      // Check which neighbors are valid (traversible and observed).
+      for (int i = 0; i < neighbors.cols(); i++) {
+        // These are columns for some reason????? Why??????????
+        const voxblox::GlobalIndex& neighbor = neighbors.col(i);
+        voxblox::EsdfVoxel* esdf_voxel =
+            layer->getVoxelPtrByGlobalIndex(neighbor);
+        if (esdf_voxel == nullptr || !esdf_voxel->observed ||
+            esdf_voxel->distance < constraints_.robot_radius) {
+          continue;
+        }
+        // Don't go backwards at least at this step.
+        if (step > 0 && neighbor == last_index) {
+          continue;
+        }
+        valid_neighbors.push_back(neighbor);
+      }
+
+      // Nowhere for us to go. :(
+      if (valid_neighbors.empty()) {
+        break;
+      }
+
+      // Select one to go to.
+      last_index = current_index;
+      double best_goal_distance = std::numeric_limits<double>::max();
+      for (const voxblox::GlobalIndex& neighbor : valid_neighbors) {
+        double neighbor_goal_distance = (goal_index - neighbor).norm();
+        if (neighbor_goal_distance < best_goal_distance) {
+          best_goal_distance = neighbor_goal_distance;
+          current_index = neighbor;
+        }
+      }
+
+      // TODO!!!! Add options other than just goal-seeking.
+    }
+
+    // Evaluate how good the current point is.
+    double current_distance = (goal_index - current_index).norm();
+    if (current_distance < best_distance) {
+      best_distance = current_distance;
+      best_index = current_index;
     }
   }
   return true;
