04af3d676081e958fa34e99143a3e5d1cdff1128
flybook4@flybook4 <flybook4@flybook4>
2019-05-15 18:31:21 +0200

    Add some more output.

--- mav_local_planner/src/mav_local_planner.cpp
+++ mav_local_planner/src/mav_local_planner.cpp
@@ -313,6 +313,10 @@ void MavLocalPlanner::avoidCollisionsTowardWaypoint() {
           std::min(path_index_ + static_cast<size_t>((replan_lookahead_sec_) /
                                                      constraints_.sampling_dt),
                    path_queue_.size());
+      ROS_INFO(
+          "[Mav Local Planner][Plan Step] Current path index: %zu Replan start "
+          "index: %zu",
+          path_index_, replan_start_index);
       // Cut out the remaining snippet of the trajectory so we can do
       // something with it.
       std::copy(path_queue_.begin() + replan_start_index, path_queue_.end(),
