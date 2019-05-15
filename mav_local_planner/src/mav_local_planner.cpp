6229592cf0a07ef625ae9eb7b87941b88cecd36f
flybook4@flybook4 <flybook4@flybook4>
2019-05-15 18:46:52 +0200

    Fix build

--- mav_local_planner/src/mav_local_planner.cpp
+++ mav_local_planner/src/mav_local_planner.cpp
@@ -166,7 +166,7 @@ void MavLocalPlanner::waypointListCallback(
 
 void MavLocalPlanner::planningTimerCallback(const ros::TimerEvent& event) {
   ROS_WARN("[Mav Planning Timer] Difference between real and expected: %d",
-           (-event.current_expected + event.current_real).toSec());
+           (event.current_real - event.current_expected).toSec());
   // Wait on the condition variable from the publishing...
   if (should_replan_.wait_for(replan_dt_)) {
     planningStep();
